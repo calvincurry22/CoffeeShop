87e8e27ee1e2262715eee493a0a926d63951f45f
calvincurry22 <calvincurry22@gmail.com>
2020-06-29 22:10:49 -0500

    initial commit

---
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShop.Models;
+using CoffeeShop.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CoffeeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        private readonly CoffeeRepository _coffeeRepository;
+        public CoffeeController(IConfiguration configuration)
+        {
+            _coffeeRepository = new CoffeeRepository(configuration);
+        }
+
+        // https://localhost:5001/api/coffeevariety/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_coffeeRepository.GetAll());
+        }
+
+        // https://localhost:5001/api/coffeevariety/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var variety = _coffeeRepository.Get(id);
+            if (variety == null)
+            {
+                return NotFound();
+            }
+            return Ok(variety);
+        }
+
+        // https://localhost:5001/api/coffeevariety/
+        [HttpPost]
+        public IActionResult Post(Coffee coffee)
+        {
+            _coffeeRepository.Add(coffee);
+            return CreatedAtAction("Get", new { id = coffee.Id }, coffee);
+        }
+
+        // https://localhost:5001/api/coffeevariety/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Coffee coffee)
+        {
+            if (id != coffee.Id)
+            {
+                return BadRequest();
+            }
+
+            _coffeeRepository.Update(coffee);
+            return NoContent();
+        }
+
+        // https://localhost:5001/api/coffeevariety/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _coffeeRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
